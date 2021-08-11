4ed06a296b5d40bbbf1e0c4f1d1bb1804d4f0afe
sunsw4 <sunsw4@lenovo.com>
2021-08-11 20:36:29 +0800

    添加闭包

---
+++ gobase/closures/closure.go
@@ -0,0 +1,6 @@
+/*
+ * @Date: 2021-08-11 20:28:52
+ * @LastEditors: seven sun
+ * @LastEditTime: 2021-08-11 20:28:52
+ * @FilePath: /面试题/gobase/closures/closure.go
+ */
---
+++ gobase/closures/closure_test.go
@@ -0,0 +1,6 @@
+/*
+ * @Date: 2021-08-11 20:29:05
+ * @LastEditors: seven sun
+ * @LastEditTime: 2021-08-11 20:29:06
+ * @FilePath: /面试题/gobase/closures/closure_test.go
+ */
