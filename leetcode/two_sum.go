1e750be94cd02dc524e471571e438cb56b58d2d0
sunsw4 <sunsw4@lenovo.com>
2021-08-14 11:31:48 +0800

    网络

--- leetcode/two_sum.go
+++ leetcode/two_sum.go
@@ -1,19 +1,18 @@
 /*
  * @Date: 2021-08-13 07:24:26
- * @LastEditors: seven sun 
- * @LastEditTime: 2021-08-13 13:56:09
- * @FilePath: /interview/leetcode/two_sum.go
+ * @LastEditors: seven sun
+ * @LastEditTime: 2021-08-13 14:29:32
+ * @FilePath: /面试题/leetcode/two_sum.go
  */
 package leetcode
 
-func Sum(nums []int, target int) []int{
-   for (i:=0;i++;i<len(nums)){
-	   for (j:=0;j++;j<len(nums)){
-		   if nums[i]+nums[j]=target{
-		   return []int{nums[i],nums[j]}
-		   }
-   }
-  }
-return []int{}
+func Sum(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{nums[i], nums[j]}
+			}
+		}
+	}
+	return []int{}
 }
-
--- leetcode/twosum_test.go
+++ leetcode/twosum_test.go
@@ -1,9 +1,22 @@
 /*
  * @Date: 2021-08-13 13:46:50
  * @LastEditors: seven sun
- * @LastEditTime: 2021-08-13 13:47:08
- * @FilePath: /interview/leetcode/twosum_test.go
+ * @LastEditTime: 2021-08-14 10:37:57
+ * @FilePath: /面试题/leetcode/twosum_test.go
  */
-func TestTwoSum() {
+package leetcode
 
+import (
+	"net"
+	"testing"
+)
+
+func TestTwoSum(t *testing.T) {
+	t.Log("hello")
+	t.Log(Sum([]int{1, 3, 4, 6, 7, 9}, 10))
+	listener, err := net.Listen("tcp", "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+	
 }
